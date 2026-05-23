802d410f8cd0a305235586efdb72c04506a651b1
matej.schonfeld <schonfeldmatej33@gmail.com>
2026-05-23 17:38:51 +0200

    feat: update content, footer info, sitemap, robots.txt and SEO meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -16,23 +16,20 @@ export default function App() {
         {/* Popis */}
         <div className="max-w-2xl text-gray-400 text-base sm:text-lg leading-relaxed font-light space-y-6 text-center">
           <p>
-            Glenford Corp s.r.o. je společnost zaměřená na správu vlastních aktiv,
-            strategické investice a rozvoj inovativních obchodních projektů s
-            mezinárodním přesahem. Společnost působí v prostředí evropského trhu
-            a při realizaci svých aktivit využívá know-how, obchodní zkušenosti a
-            kapitálové zázemí svých zahraničních partnerů, vycházející z
-            mezinárodní spolupráce a dlouhodobých partnerství.
+            Glenford Corp s.r.o. je společnost opřená o zahraniční kapitálové spojení, zaměřená
+            na správu vlastních aktiv, strategické investice a rozvoj inovativních obchodních
+            projektů s mezinárodním přesahem. Společnost působí na evropském trhu a při
+            realizaci svých aktivit využívá know-how, obchodní zkušenosti a kapitálové zázemí
+            svých zahraničních partnerů, vycházející z mezinárodní spolupráce a dlouhodobých
+            partnerství.
           </p>
           <p>
-            Obchodní filozofie společnosti je postavena na efektivitě, strategickém
-            řízení a schopnosti identifikovat příležitosti tam, kde tradiční obchodní
-            modely často narážejí na vysoké provozní náklady nebo omezené tržní
-            možnosti. Díky propojení na mezinárodní obchodní struktury a
-            zahraniční kapitálové vazby je společnost schopna rozvíjet projekty s
-            ambiciózní dlouhodobou vizí a stabilním finančním zázemím.
+            Obchodní filozofie společnosti je postavena na efektivitě, strategickém řízení a
+            schopnosti identifikovat příležitosti tam, kde tradiční obchodní modely často narážejí
+            na vysoké provozní náklady nebo nevhodné tržní možnosti.
           </p>
           <p>
-            Součástí portfolia společnosti je mimo jiné projekt{' '}
+            V České Republice je součástí portfolia společnosti mimo jiné projekt{' '}
             <a
               href="https://nejlevnejsi-skoda.cz"
               target="_blank"
@@ -41,18 +38,21 @@ export default function App() {
             >
               nejlevnejsi-skoda.cz
             </a>
-            , zaměřený na prodej nových vozů Škoda v České republice za
-            mimořádně výhodných podmínek. Projekt využívá efektivní provozní
-            model, nízké režijní náklady a především široké obchodní možnosti
-            celého trhu EU, díky čemuž dokáže zákazníkům nabídnout atraktivní
-            cenové podmínky při zachování standardních záručních podmínek a
-            profesionálního přístupu.
+            , zaměřený na prodej nových vozů Škoda v ČR za mimořádně výhodných
+            podmínek. Efektivní provozní model, nízké režijní náklady a především široké
+            obchodní možnosti celé EU, díky čemuž zákazníkům nabízí atraktivní cenové
+            podmínky při zachování standardních záručních podmínek a profesionálního
+            přístupu.
           </p>
           <p>
-            Glenford Corp s.r.o. dlouhodobě usiluje o budování stabilních a
-            perspektivních obchodních projektů s důrazem na transparentnost,
-            profesionalitu a vytváření dlouhodobé hodnoty pro zákazníky i obchodní
-            partnery.
+            Druhotně se společnost zaměřuje na pronájem vlastních nemovitostí, případně
+            nemovitostí svých obchodních partnerů.
+          </p>
+          <p>
+            Glenford Corp s.r.o. jako nástupnická organizace své evropské mateřské společnosti,
+            bude dlouhodobě usilovat o budování stabilních a perspektivních obchodních
+            projektů s důrazem na transparentnost, profesionalitu a vytváření dlouhodobých
+            hodnot pro zákazníky a obchodní partnery.
           </p>
         </div>
       </header>
@@ -80,6 +80,8 @@ export default function App() {
           Glenford Corp s.r.o.&nbsp;&nbsp;·&nbsp;&nbsp;IČ: 24602400
           <br />
           Primátorská 296/38, Libeň (Praha 8), 180 00 Praha
+          <br />
+          ID datové schránky: 8xq2ue8&nbsp;&nbsp;·&nbsp;&nbsp;Číslo bankovního účtu: 368 512 688 / 0300 nebo 7064728369 / 0800
         </p>
       </footer>
 
